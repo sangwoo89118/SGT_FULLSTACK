b56f79c72bfb40af39d98a8096ceaa58e4105e96
sangwoo89118 <sangwoo89118@gmail.com>
2018-02-03 05:01:06 -0800

    completed rendering average on header component

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class Header extends Component {
+class Header extends Component {
+
+
     render() {
+
         return (
             <div className="page-header">
                 <h1 className="hidden-xs">Student Grade Table
-                    <small className="pull-right">Grade Average : <span className="avgGrade label label-default" >0</span></small>
+                    <small className="pull-right">Grade Average : <span className="avgGrade label label-default" >{this.props.average}</span></small>
                 </h1>
                 <h3 className="visible-xs">Student Grade Table
-                    <small className='pull-right'>Grade Average : <span className="avgGrade label label-default">0</span></small>
+                    <small className='pull-right'>Grade Average : <span className="avgGrade label label-default">{this.props.average}</span></small>
                 </h3>
             </div>
         );
     }
 }
+
+
+function mapStateToProps(state){
+    return{
+        average: state.students.average
+    }
+}
+
+export default connect(mapStateToProps)(Header);
--- client/src/reducers/students_reducer.js
+++ client/src/reducers/students_reducer.js
@@ -2,13 +2,18 @@
 import types from '../actions/types';
 
 const DEFAULT_STATE={
-    list : []
+    list : [],
+    average: null
 }
 
 export default function(state=DEFAULT_STATE, action){
     switch(action.type){
         case types.GET_STUDENTS:
-            return {list: action.payload.data.data}
+            const data = action.payload.data.data;
+            const average = data.reduce((total, list)=> total + parseInt(list.grade), 0) / data.length;
+
+
+            return {list: data, average: Math.round(average)}
         default:
             return state;
     }
