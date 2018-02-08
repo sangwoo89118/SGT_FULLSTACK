1ef51dfda5eccc0952f926cd987bd22ad1d63a6f
sangwoo89118 <sangwoo89118@gmail.com>
2018-02-08 12:37:07 -0800

    changed port for test.js

--- server/node/test.js
+++ server/node/test.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
 const server = express();
+const port = process.env.PORT || '4000';
+
 
 server.get('/test', function(req, res){
     res.send('<h1>working</h1>');
 })
 
-server.listen(4000, function(){
-    console.log('server is running on port 4000');
+server.listen(port, function(){
+    console.log('server is running on port', port);
 })
