12121e512602a5081c8fc0912b3027de71afe094
sangwoo89118 <sangwoo89118@gmail.com>
2018-02-16 14:40:00 -0800

    change outputPath for css files file loader

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -36,7 +36,7 @@ module.exports = {
                 use: {
                     loader: 'file-loader',
                     options: {
-                        outputPath: 'assets/fonts/'
+                        outputPath: '/assets/fonts/'
                     }
                 }
             }
